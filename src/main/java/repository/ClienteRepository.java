70a17681dea2b007578923f4c78325bde6988c33
filipe.dias <fedispato@gmail.com>
2024-02-02 22:44:44 -0300

    fix: ajuste na inicializacao do projeto

--- src/main/java/repository/ClienteRepository.java
+++ src/main/java/repository/ClienteRepository.java
@@ -2,8 +2,6 @@ package repository;
 
 import model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ClienteRepository extends JpaRepository<Cliente,Long> {
 }
